34ee09f1174ec4071650516b04838402bb74b7c6
CJHart02 <carlton.hart@gmail.com>
2016-02-01 10:16:52 -0500

    Create Volume_Surface_Area_Cylinder.cpp

---
+++ Volume_Surface_Area_Cylinder.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+double pi = 3.141593
+double radius;
+double height;
+double volume;
+double surface;
+
+int main()
+{
+  cout << " This program will allow you to calculate the volume and surface area of a cylinder. " << endl;
+  cout << " Please enter the radius: ";
+  cin >> radius;
+  cout << " Please enter the height: ";
+  cin >> height;
+  double volume = pi * (radius * radius) * height;
+  double surface = (2.0 * pi * radius * height) + ( 2.0 * pi * (radius * radius)); 
+  cout << " When the radius of a cylinder is " << radius << " inches and its height is " << height << " inches, the volume is " << volume << " cubic inches and the surface area is " << surface << " cubic inches." << endl;
+  return 0;
+}
